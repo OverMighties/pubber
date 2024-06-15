ae6a4cabd7e517ea0fe159d3d46d226b9c0b04e5
Mistrz Karp <debrujne2@gmail.com>
2024-06-15 13:41:39 +0200

    SearcherFragment ui feat and top bar deletion

---
+++ app/src/main/java/com/overmighties/pubber/util/DimensionsConverter.java
@@ -0,0 +1,15 @@
+package com.overmighties.pubber.util;
+
+
+import android.content.Context;
+
+public class DimensionsConverter {
+
+    public static float dpFromPx(final Context context, final float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    public static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+}
